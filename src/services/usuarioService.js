d9e97b9ca709f195521819e6212e3584cbd444af
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-06 16:41:51 -0300

    encriptação de senha no service

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -1,42 +1,46 @@
 import { prisma } from "../utils/prisma.js";
+import bcrypt from "bcrypt";
 
-export const Usuario = {
+export const usuarioService = {
+	listarUsuarios: async () => {
+		return await prisma.usuarios.findMany();
+	},
 
-    listarUsuarios: async() => {
-        return await prisma.usuario.findMany();
-    },
+	buscarUsuarioPorId: async (id) => {
+		return await prisma.usuarios.findUnique({
+			where: {
+				id: Number(id)
+			}
+		});
+	},
 
-    buscarUsuarioPorId: async(id) => {
-        return await prisma.usuario.findUnique({
-            where: {
-                id
-            }
-        })
-    },
+	criarUsuario: async (nome, email, senha, telefone, tipo) => {
+		const passwordEncripitado = await bcrypt.hash(senha, 10);
 
-    criarUsuario: async(nome, email, senha, telefone, tipo) => {
-        return await prisma.usuario.create({
-            data: {
-                nome,
-                email,
-                senha,
-                telefone,
-                tipo
-            }
-        });
-    },
+		return await prisma.usuarios.create({
+			data: {
+				nome,
+				email,
+				senha: passwordEncripitado,
+				telefone,
+				tipo
+			}
+		});
+	},
 
-    atualizarUsuario: async(id, nome, email, senha, telefone, tipo) => {
-        return await prisma.usuario.update({
-            where: { id },
-            data: { nome, email, senha, telefone, tipo }
-        });
-    },
+	atualizarUsuario: async (id, nome, email, senha, telefone, tipo) => {
+		const passwordEncripitado = await bcrypt.hash(senha, 10);
 
-    deletarUsuario: async(id) => {
-        return await prisma.usuario.delete({
-            where: { id }
-        });
-    }
+		console.log(id, nome, email, senha, telefone, tipo);
+		return await prisma.usuarios.update({
+			where: { id: Number(id) },
+			data: { nome, email, senha: passwordEncripitado, telefone, tipo }
+		});
+	},
 
-}
+	deletarUsuario: async (id) => {
+		return await prisma.usuarios.delete({
+			where: { id: Number(id) }
+		});
+	}
+};
