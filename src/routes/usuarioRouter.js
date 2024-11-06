790f756a5ec99899e528a5be2b47bb305c665da9
Bruno Silva <brunosilvabrait@gmail.com>
2024-11-06 16:42:24 -0300

    rota de login, falta importar o middleware e alinhar nas rotas que devem ser restritas

--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -1,29 +1,33 @@
 import { Router } from "express";
 import { usuarioController } from "../controllers/usuarioController.js";
+import { loginController } from "../controllers/loginController.js";
 
 export const userRouter = () => {
-    const router = Router();
+	const router = Router();
 
-    router.get('/usuario', async (req, res) => {
-        return await usuarioController.listarUsuarios(req, res);
-    })  
+	router.post("/login", async (req, res) => {
+		return await loginController.loginUsuario(req, res);
+	});
 
-    router.get('/usuario/:id', async (req, res) => {
-        return await usuarioController.buscarUsuarioPorId(req, res);
-    })
+	router.get("/usuario", async (req, res) => {
+		return await usuarioController.listarUsuarios(req, res);
+	});
 
-    router.post('/usuario', async (req, res) => {
-        return await usuarioController.criarUsuario(req, res);
-    })
+	router.get("/usuario/:id", async (req, res) => {
+		return await usuarioController.buscarUsuarioPorId(req, res);
+	});
 
-    router.put('/usuario/:id', async (req, res) => {
-        return await usuarioController.atualizarUsuario(req, res);
-    })
+	router.post("/usuario", async (req, res) => {
+		return await usuarioController.criarUsuario(req, res);
+	});
 
-    router.delete('/usuario/:id', async (req, res) => {
-        return await usuarioController.deletarUsuario(req, res);
-    })
+	router.put("/usuario/:id", async (req, res) => {
+		return await usuarioController.atualizarUsuario(req, res);
+	});
 
-    return router;
+	router.delete("/usuario/:id", async (req, res) => {
+		return await usuarioController.deletarUsuario(req, res);
+	});
 
-}
+	return router;
+};
